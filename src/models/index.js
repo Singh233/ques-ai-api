9d7e80660ce7a4795a5b630af32c710f1aa99017
Sanambir Singh <Sanambir123@gmail.com>
2025-04-26 13:28:58 +0530

    feat: implement email and user models with validation and plugins

---
+++ src/models/index.js
@@ -0,0 +1,4 @@
+const User = require('./user.model.js');
+const Email = require('./email.model.js');
+
+module.exports = { User, Email };
---
+++ src/models/user.model.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+const { toJSON, paginate, softDelete } = require('./plugins/index.js');
+
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+    },
+    email: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Email',
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    type: {
+      type: String,
+      enum: ['admin', 'user'],
+      default: 'user',
+    },
+  },
+  { timestamps: true },
+);
+
+userSchema.plugin(toJSON);
+userSchema.plugin(paginate);
+userSchema.plugin(softDelete);
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
