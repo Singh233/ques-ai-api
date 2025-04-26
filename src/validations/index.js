c05795b24b604de6ee9afe731ee275e741d8bf0a
Sanambir Singh <Sanambir123@gmail.com>
2025-04-26 14:42:01 +0530

    feat: add validation schemas for authentication and user management

---
+++ src/validations/index.js
@@ -0,0 +1,4 @@
+const authValidation = require('./auth.validation.js');
+const userValidation = require('./user.validation.js');
+
+module.exports = { authValidation, userValidation };
---
+++ src/validations/user.validation.js
@@ -0,0 +1,79 @@
+const Joi = require('joi');
+const { objectId } = require('./custom.validation.js');
+const { Types } = require('../config/accessControl.js');
+
+const getUsers = {
+  query: Joi.object().keys({
+    name: Joi.string(),
+    sortBy: Joi.string(),
+    limit: Joi.number().integer(),
+    page: Joi.number().integer(),
+    populate: Joi.string(),
+  }),
+};
+
+const getUser = {
+  params: Joi.object().keys({
+    id: Joi.string().custom(objectId).required(),
+  }),
+};
+
+const searchUsers = {
+  params: Joi.object().keys({
+    searchQuery: Joi.string().required(),
+  }),
+  query: Joi.object().keys({
+    sortBy: Joi.string(),
+    limit: Joi.number().integer(),
+    page: Joi.number().integer(),
+    populate: Joi.string(),
+    types: Joi.array().items(Joi.string().valid(...Object.values(Types))),
+  }),
+};
+
+const createUser = {
+  body: Joi.object().keys({
+    name: Joi.string().required(),
+    email: Joi.string().required(),
+    type: Joi.string()
+      .valid(...Object.values(Types))
+      .required(),
+  }),
+};
+
+const updateUser = {
+  params: Joi.object().keys({
+    id: Joi.required().custom(objectId),
+  }),
+  body: Joi.object()
+    .keys({
+      name: Joi.string(),
+      type: Joi.string().valid(...Object.values(Types)),
+    })
+    .min(1),
+};
+
+const changeType = {
+  body: Joi.object().keys({
+    id: Joi.string().custom(objectId).required(),
+    type: Joi.string()
+      .valid(...Object.values(Types))
+      .required(),
+  }),
+};
+
+const deleteUser = {
+  params: Joi.object().keys({
+    id: Joi.string().custom(objectId).required(),
+  }),
+};
+
+module.exports = {
+  getUsers,
+  getUser,
+  searchUsers,
+  createUser,
+  updateUser,
+  changeType,
+  deleteUser,
+};
