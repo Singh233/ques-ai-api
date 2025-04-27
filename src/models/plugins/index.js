3513d5d82308eaa8f7fe7c1b2b4db5da7c7ef6af
Sanambir Singh <Sanambir123@gmail.com>
2025-04-27 13:27:57 +0530

    fix: correct casing of toJSON plugin import

--- src/models/plugins/index.js
+++ src/models/plugins/index.js
@@ -1,5 +1,5 @@
 const paginate = require('./paginate.plugin.js');
-const toJSON = require('./toJSON.plugin.js');
+const toJSON = require('./toJson.plugin.js');
 const cascadeDelete = require('./cascadeDelete.plugin.js');
 const softDelete = require('./softDelete.plugin.js');
 
